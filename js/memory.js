de480953e198d67aae5301ef1a7620b5d788b4d4
Lars Laade <lars.laade@googlemail.com>
2014-12-26 23:45:57 +0100

    remove jQuery dependency

--- js/memory.js
+++ js/memory.js
@@ -1,129 +1,209 @@
 // Memory
 
-// (c) 2010-2013 Lars Laade
+// (c) 2010-2014 Lars Laade
 // MIT license
 
-var Memory = function(list_element) {
-	var that = this;
-	this.list = list_element;
-	this.page = $('.page');
-	this.items = this.list.find('.card');
-	this.items_length = this.items.length;
-	this.current_flipped_1 = null;
-	this.current_flipped_2 = null;
-	this.match = 0;
-	this.rounds = 0;
-	this.win = $(window);
+(function() {
 
-	this.has3d = Modernizr.csstransforms3d;
+	var list = document.getElementById('card_list');
 
-	this.list.on('click', 'li', function() {
-		that.flipCard($(this));
-	}).on('mousedown', 'li', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-	});
-	this.sort();
-	this.resize();
-	this.win.on('resize', function() {
-		that.resize();
-	});
-};
+	var page = document.getElementsByClassName('page')[0];
+	var items = document.getElementsByClassName('card');
 
-Memory.prototype.resize = function() {
-	var h = this.win.height(),
-		w = this.win.width(),
-		new_w = h;
-	if (new_w > w) {
-		new_w = w;
-	}
-	this.list.width(new_w).height(new_w);
-};
+	var itemsCount = items.length;
+	var currentFlippedOne = null;
+	var currentFlippedTwo = null;
+	var match = 0;
+	var rounds = 0;
 
-Memory.prototype.flipCard = function(card) {
-	var that = this;
-	if (card.hasClass('flipped') === false) {
-		if (!this.current_flipped_1) {
-			card.toggleClass('flipped');
-			this.current_flipped_1 = card;
-		} else if (!this.current_flipped_2) {
-			card.toggleClass('flipped');
-			this.current_flipped_2 = card;
-			setTimeout(function() {
-				that.check();
-			}, (this.has3d ? 1000 : 100));
+	var has3d = Modernizr.csstransforms3d;
+
+	var resizeHandler = function() {
+		var height = document.body.scrollHeight,
+			width = document.body.scrollWidth,
+			newWidth = height;
+
+		if (newWidth > width) {
+			newWidth = width;
 		}
-	}
-};
 
-Memory.prototype.check = function() {
-	var that = this,
-		div;
-	this.rounds++;
-	// not the same
-	if (this.current_flipped_1.find('.front').attr('src') !== this.current_flipped_2.find('.front').attr('src')) {
-		this.current_flipped_1.addClass('fail');
-		this.current_flipped_2.addClass('fail');
+		list.style.width = newWidth + 'px';
+		list.style.height = newWidth + 'px';
+	};
+
+	var flipCard = function(card) {
+
+		if (card.classList.contains('flipped')) {
+			return;
+		}
+
+		if (!currentFlippedOne) {
+			card.classList.toggle('flipped');
+			currentFlippedOne = card;
+		} else if (!currentFlippedTwo) {
+			card.classList.toggle('flipped');
+			currentFlippedTwo = card;
+
+			setTimeout(function() {
+				check();
+			}, (has3d ? 1000 : 100));
+		}
+	};
+
+	var retryHandler;
+
+	var check = function() {
+		var div;
+
+		rounds++;
+
+		// not the same
+		if (currentFlippedOne.getElementsByClassName('front')[0].title !== currentFlippedTwo.getElementsByClassName('front')[0].title) {
+			currentFlippedOne.classList.add('fail');
+			currentFlippedTwo.classList.add('fail');
+
+			setTimeout(function() {
+				fail();
+			}, 1000);
+		} else { // the same
+			currentFlippedOne.classList.add('win');
+			currentFlippedTwo.classList.add('win');
+			currentFlippedOne = null;
+			currentFlippedTwo = null;
+			match += 2;
+
+			// check if all cards are flipped
+			if (match === itemsCount) {
+				list.classList.add('rotate');
+
+				div = document.createElement('div');
+				div.classList.add('overlay');
+				div.innerHTML = 'Comparisons: <strong>' + rounds + '</strong>.<br/>Try again!';
+				div.style.top = '50%';
+				div.style.left = '50%';
+				div.style.marginTop = -150 + 'px';
+				div.style.marginLeft = -150 + 'px';
+
+				retryHandler = function() {
+					retry(div);
+				};
+				div.addEventListener('click', retryHandler);
+
+				list.parentElement.appendChild(div);
+
+				setTimeout(function() {
+					list.classList.remove('rotate');
+				}, 2000);
+			}
+		}
+	};
+
+	var retry = function(element) {
+
+		var i = itemsCount -1,
+			item;
+
+		element.removeEventListener('click', retryHandler);
+		element.remove();
+
+		list.classList.remove('rotate');
+
+		do {
+			item = items[i];
+			item.classList.remove('win');
+			item.classList.remove('flipped');
+			i--;
+		} while (i > -1)
+
 		setTimeout(function() {
-			that.fail();
+			sort();
 		}, 1000);
-	} else { // the same
-		this.current_flipped_1.addClass('win');
-		this.current_flipped_2.addClass('win');
-		this.current_flipped_1 = null;
-		this.current_flipped_2 = null;
-		this.match += 2;
-		// check if all cards are flipped
-		if (this.match === this.items_length) {
-			this.list.addClass('rotate');
-			div = $('<p class="overlay"></p>').html('Comparisons: <strong>' + this.rounds + '</strong>.<br/>Try again!').css({
-				'top': this.page.height() / 2 - 150,
-				'left': this.page.width() / 2 - 150
-			}).on('click', function() {
-				that.retry(div);
-			});
-			this.list.parent().append(div);
-			setTimeout(function() {
-				that.list.removeClass('rotate');
-			}, 2000);
+	};
+
+	var fail = function() {
+		currentFlippedOne.classList.remove('fail');
+		currentFlippedOne.classList.toggle('flipped');
+
+		currentFlippedTwo.classList.remove('fail');
+		currentFlippedTwo.classList.toggle('flipped');
+
+		currentFlippedOne = null;
+		currentFlippedTwo = null;
+	};
+
+	var sort = function() {
+		var item,
+			done = [],
+			sorted = false,
+			n = null,
+			l = itemsCount;
+
+		rounds = 0;
+		match = 0;
+
+		do {
+			n = Math.floor(Math.random() * (l + 1));
+
+			console.log(n);
+
+			if (done.indexOf(n) === -1) {
+				item = items[n];
+				console.log(item);
+				if (item) {
+					item.remove();
+					list.appendChild(item);
+				}
+				done.push(n);
+			}
+		} while (done.length <= l);
+	};
+
+	sort();
+	resizeHandler();
+	window.addEventListener('resize', resizeHandler);
+
+	var getParent = function(element, search) {
+		var p = element.parentElement;
+
+		if (!p || !p.nodeName) {
+			return null;
 		}
-	}
-};
 
-Memory.prototype.retry = function(element) {
-	var that = this,
-		i = 0,
-		item;
-	element.unbind('click').remove();
-	this.list.removeClass('rotate');
-	this.items.removeClass('win flipped');
-	setTimeout(function() {
-		that.sort();
-	}, 1000);
-};
-
-Memory.prototype.fail = function() {
-	this.current_flipped_1.removeClass('fail').toggleClass('flipped');
-	this.current_flipped_2.removeClass('fail').toggleClass('flipped');
-	this.current_flipped_1 = null;
-	this.current_flipped_2 = null;
-};
-
-Memory.prototype.sort = function() {
-	var done = new Array(),
-		sorted = false,
-		n = null,
-		l = this.items_length;
-	this.list.empty();
-	this.rounds = 0;
-	this.match = 0;
-	do {
-		n = Math.floor(Math.random() * (l + 1));
-		if ($.inArray(n, done) === -1) {
-			done[done.length] = n;
-			this.list.append(this.items[n]);
+		if (p.nodeName.toLowerCase() === search) {
+			return p;
+		} else {
+			return getParent(p, search);
 		}
-	} while (done.length <= l);
-};
+	};
 
-var mem = new Memory($('#card_list'));
+	// clicking a card
+	list.addEventListener('click', function(event) {
+		var element = event.target,
+			nodeName = element.nodeName.toLowerCase();
+
+		if (nodeName !== 'li') {
+			element = getParent(element, 'li');
+			nodeName = element.nodeName.toLowerCase();
+		}
+
+		if (nodeName === 'li') {
+			flipCard(element);
+		}
+	});
+
+	// prevent dragging
+	list.addEventListener('mousedown', function(event) {
+		var element = event.target,
+			nodeName = element.nodeName.toLowerCase();
+
+		if (nodeName !== 'li') {
+			element = getParent(element, 'li');
+			nodeName = element.nodeName.toLowerCase();
+		}
+
+		if (nodeName === 'li') {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+	});
+})();
